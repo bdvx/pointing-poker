711e273bc7f550a06366688b39291577ebc44f3a
JxmJuice <pro100alex17@gmail.com>
2021-09-05 21:42:12 +0300

    feat: message component

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,23 +1,13 @@
-import { Avatar, Toolbar, Tooltip } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import SendIcon from "@material-ui/icons/Send";
+import ChatMessage from "../ChatMessage/ChatMessage";
 import "./Chat.scss";
 
 const Chat = () => {
   return (
     <div className="Chat">
       <ul className="Chat_messages">
-        <li className="Chat_message">
-          <div className="Chat_message__text">
-            Message long long long long longlonglonglonglonglonglong long long
-            long long long long long long long long long long long long long
-            long long long long long long long long long long long long long
-            long long long long long long long long
-          </div>
-          <Tooltip title={<div style={{textAlign: 'center'}}>Harry Lewis<br/>Senior Software Engineer</div>}>
-            <Avatar className="Chat_message__avatar">H</Avatar>
-          </Tooltip>
-        </li>
+        <ChatMessage message='Random message' name='Harry' surname='' src='./logo192.png' position='Middle Software Engineer'/>
       </ul>
       <div className="Chat_input">
         <input placeholder="Type something..." className="Chat_input__text" />
