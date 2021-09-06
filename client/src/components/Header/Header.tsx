37acd55542d9361994c582c59d639812c2da4757
JxmJuice <pro100alex17@gmail.com>
2021-09-06 19:50:45 +0300

    feat: header + routing

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { Link, Route, Switch } from "react-router-dom";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <header className="Header">
+        <div className="Header_wrapper">
+          <div className="Header_logo"></div>
+          <Link to="/home" className="Header_link">Home</Link>
+          <Link to="/about" className="Header_link">About</Link>
+        </div>
+      </header>
+      <Switch>
+        <Route path="/about">About</Route>
+        <Route path="/home">Home</Route>
+      </Switch>
+    </>
+  );
+};
+
+export default Header;
