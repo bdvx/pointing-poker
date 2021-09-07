cab739ac9486382ecb9d1bc8c0ffff993ec2c0cb
WFZ1 <pavelskumenko@gmail.com>
2021-09-07 17:51:31 +0300

    feat: delete Box element and rewrite using scss file

--- client/src/components/welcome-page/Welcome-page.tsx
+++ client/src/components/welcome-page/Welcome-page.tsx
@@ -1,68 +1,25 @@
 import './Welcome-page.scss';
-import { Box, Button, Grid, TextField } from '@material-ui/core';
-import pokerCardsImg from '../../assets/icons/poker-cards.svg';
 import { FC } from 'react';
-import { bp } from '../../material-ui-variables';
-
-declare module '@material-ui/core/Box' {
-  interface BoxProps {
-    src?: string;
-    alt?: string
-  }
-}
+import { Button, Grid, TextField } from '@material-ui/core';
+import pokerCardsImg from '../../assets/icons/poker-cards.svg';
 
 export const WelcomePage: FC<{classes: string}> = ({classes}: {classes: string}) => {
   return (
     <div className={`welcome-page ${ classes }`}>
-      <Box
-        className="welcome-page__header"
-        sx={{
-          [bp.up('phone')]: {
-            display: 'flex',
-            justifyContent: 'center'
-          }
-        }}
-      >
-        <Box
-          component="img"
+      <div className="welcome-page__header">
+        <img
           className="welcome-page__header-logo"
           src={pokerCardsImg}
           alt="Poker cards"
-          sx={{
-            [bp.down(bp.values.phone)]: {
-              display: 'block',
-              margin: 'auto'
-            },
-            [bp.up('phone')]: {
-              marginTop: -60,
-              marginRight: 25
-            }
-          }}
         />
 
-        <Box
-          className="welcome-page__header-heading"
-          sx={{
-            [bp.down(bp.values.phone)]: {
-              margin: 'auto',
-              '&::before': {
-                display: 'none'
-              }
-            }
-          }}
-        >
+        <div className="welcome-page__header-heading">
           <span className="welcome-page__header-title welcome-page__header-title-1">Poker</span>
           <span className="welcome-page__header-title welcome-page__header-title-2">Planning</span>
-        </Box>
-      </Box>
+        </div>
+      </div>
 
-      <Box
-        className="welcome-page__subtitle"
-        component="span"
-        color={'secondary.main'}
-      >
-        Start your planning:
-      </Box>
+      <span className="welcome-page__subtitle" color={'secondary.main'}>Start your planning:</span>
 
       <Grid
         className="welcome-page__control-block"
@@ -70,17 +27,7 @@ export const WelcomePage: FC<{classes: string}> = ({classes}: {classes: string})
         justifyContent="space-between"
         alignItems="center"
       >
-        <Box
-          className="welcome-page__control-block-text"
-          component="span"
-          sx={{
-            [bp.down(bp.values['phone-xl'])]: {
-              marginBottom: 15
-            }
-          }}
-        >
-          Create session:
-        </Box>
+        <span className="welcome-page__control-block-text">Create session:</span>
         <Button
           className="welcome-page__control-block-btn"
           variant="contained"
@@ -91,23 +38,12 @@ export const WelcomePage: FC<{classes: string}> = ({classes}: {classes: string})
         </Button>
       </Grid>
 
-      <Box
-        className="welcome-page__subtitle welcome-page__subtitle-2"
-        component="span"
-        color={'secondary.main'}
-        sx={{
-          [bp.up(bp.values['phone-xl'])]: {
-            marginLeft: 200
-          }
-        }}
-      >
-        OR:
-      </Box>
+      <span className="welcome-page__subtitle welcome-page__subtitle_margin_left" color={'secondary.main'}>OR:</span>
 
       <div className="welcome-page__control-block">
         <span className="welcome-page__control-block-text welcome-page__control-block-text_block">
           Connect to lobby by { }
-          <Box component="strong" color={'secondary.main'}>URL</Box>
+          <strong color={'secondary.main'}>URL</strong>
           :
         </span>
 
