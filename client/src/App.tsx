be81cb4be349db0a13c445cc652eda459e6d0b8d
WFZ1 <pavelskumenko@gmail.com>
2021-09-07 17:27:19 +0300

    feat: delete header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.scss';
 import { ThemeProvider } from '@material-ui/core';
-import { Header } from './components/header/Header';
 import { WelcomePage } from './components/welcome-page/Welcome-page';
 import { theme } from './material-ui-variables';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Header />
         <main>
           <WelcomePage classes="app__welcome-page" />
         </main>
