1cb2314d369042f7498a12665bde03adfb7e6d89
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2021-09-14 17:14:28 +0300

    fix: SingIn function server side

--- server/src/http.ts
+++ server/src/http.ts
@@ -1,5 +1,3 @@
-import { RegistrationModel } from "./models/httpModels/registrationModel";
-import { SignInModel } from "./models/httpModels/signInModel";
 import { Request, Response } from 'express';
 import DataService from "./tools/dataService";
 
@@ -10,21 +8,6 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-/* const users: Array<RegistrationModel> = []; */
-const testUserReg:RegistrationModel = {
-  firstName:'fName',
-  jobPosition:"JOb",
-  lastName:"lName",
-  login:"login1",
-  password:"hash123",
-  //role:"role",
-  avatar:"avatar",
-  id:123
-} 
-const testUserSign: SignInModel = {
-  login:"login1",
-  password:"hash123"
-}
 
 async function regNewUser(req:Request, res:Response) {
   const user = req.body;
@@ -35,21 +18,20 @@ async function regNewUser(req:Request, res:Response) {
 
   } else {
     res.statusCode = 500;
-    res.send(JSON.stringify(response));
+    res.send(response);
   }
 }
 
 async function signIn(req:Request, res:Response) {
-/*   const userInfo = (JSON.parse(req.body) as SignInModel); */
-  const userInfo = testUserSign;
+  const userInfo = req.body;
   const response = await DataService.signInUser(userInfo);
 
   if(response.isSuccess) {
     res.statusCode = 200;
-    res.send(JSON.stringify(response));
+    res.send(response);
   } else {
     res.statusCode = 404;
-    res.send(JSON.stringify(response));
+    res.send(response);
   }
 }
 
