dff2101b9e2ed449d8327f853df7446f872f50bd
JxmJuice <pro100alex17@gmail.com>
2021-09-14 13:48:03 +0300

    fix: app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Header from './components/Header/Header';
-import Lobby from './components/Lobby/Lobby';
+import Header from "./components/Header/Header";
+import Lobby from "./components/Lobby/Lobby";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header/>
-        <Lobby/>
-      </div>
+      <div className="App"></div>
     </BrowserRouter>
   );
 }
