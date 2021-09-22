3ee02b96642335547d57895bb578a17bdbfc1b3a
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2021-09-22 22:16:23 +0300

    RETURNES AS IT WAS

--- server/build/client/src/components/pages/AboutPage/aboutPage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AboutPage = void 0;
-function AboutPage() {
-    return <h1>About Page</h1>;
-}
-exports.AboutPage = AboutPage;
-//# sourceMappingURL=aboutPage.js.map
